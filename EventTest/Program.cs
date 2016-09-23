88226079c5e932ec98e6076400fba56b5936cd69
mike <mike@V-DOXIE-W10>
2016-09-23 17:20:08 +0800

    Add project files.

---
+++ EventTest/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventTest
+{
+    public delegate void PersonChanged();
+    class Person
+    {
+        private int age;
+        private int height;
+        private int weight;
+        
+        public event PersonChanged ChangePerson;
+
+        public void OnPersionChanged()
+        {
+            if (ChangePerson != null)
+            {
+                ChangePerson();
+            }
+        }
+        public void setAge(int mage)
+        {
+            age = mage;
+            OnPersionChanged();
+        }
+
+        public int Age
+        {
+            get{ return age;}
+            set { OnPersionChanged(); age = value;}
+        }
+       public int Height
+       {
+            get { return height; }
+            set { OnPersionChanged(); height = value; }
+       }
+        public int Weight
+        {
+            get { return weight; }
+            set { OnPersionChanged(); weight = value; }
+        }
+    }
+    class EventTest
+    {
+
+        class Listenter
+        {
+            public Listenter(Person p1)
+            {
+                // Add "ListChanged" to the Changed event on "List".
+                p1.ChangePerson += new PersonChanged(PChanged);
+            }
+
+            private void PChanged()
+            {
+                Console.WriteLine("Hello person changed");
+            }
+        }
+        public static void Main()
+        {
+
+            Person p = new Person();
+            Listenter listenter = new Listenter(p);
+            p.Age = 19;
+            Console.ReadLine();
+        }
+       
+         
+    }
+}
---
+++ EventTest/TestEvents .cs
@@ -0,0 +1,49 @@
+﻿using System;
+namespace TestEvents
+{
+    using MyCollections;
+
+    class EventListener
+    {
+        private ListWithChangedEvent List;
+
+        public EventListener(ListWithChangedEvent list)
+        {
+            List = list;
+            // Add "ListChanged" to the Changed event on "List".
+            List.Changed += new ChangedEventHandler(ListChanged);
+        }
+
+        // This will be called whenever the list changes.
+        private void ListChanged(object sender, EventArgs e)
+        {
+            Console.WriteLine("This is called when the event fires.");
+        }
+
+        public void Detach()
+        {
+            // Detach the event and delete the list
+            List.Changed -= new ChangedEventHandler(ListChanged);
+            List = null;
+        }
+    }
+
+    class Test
+    {
+        // Test the ListWithChangedEvent class.
+        //public static void Main()
+        //{
+        //    // Create a new list.
+        //    ListWithChangedEvent list = new ListWithChangedEvent();
+
+        //    // Create a class that listens to the list's change event.
+        //    EventListener listener = new EventListener(list);
+
+        //    // Add and remove items from the list.
+        //    list.Add("item 1");
+        //    list.Clear();
+        //    listener.Detach();
+        //    Console.ReadLine();
+        //}
+    }
+}
